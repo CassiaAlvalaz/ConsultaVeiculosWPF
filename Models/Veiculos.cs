9e514f98a5da922da4375c5b1c79a8c67539af0d
CassiaAlvalaz <cassia_alvalaz@hotmail.com>
2024-07-26 16:08:42 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Veiculos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsultaVeiculos.Models
+{
+    public class Veiculos
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Modelo { get; set; }
+        public short Ano { get; set; }
+
+        public List<Veiculos> ObterVeiculos()
+        {
+            List<Veiculos> listaVeiculos = new List<Veiculos>()
+            {
+                new Veiculos { Id = 1, Nome = "FORD FIESTA", Modelo = "1.0 MPI PERSONNALITÉ SEDAN 4P", Ano = 2005 },
+                new Veiculos { Id = 2, Nome = "HONDA CRV", Modelo = "2.0 LX 4X2 16V 4P", Ano = 2018 },
+                new Veiculos { Id = 3, Nome = "LAND ROVER DISCOVERY 4", Modelo = "3.0 SE 4X4 V6 24V BI-TURBO 4P", Ano = 2021 }
+            };
+
+            return listaVeiculos;
+        }
+
+        public List<Veiculos> ObterVeiculos(string filtro)
+        {
+            var listaFiltrada = from c in ObterVeiculos()
+                                where c.Nome.ToUpper().Contains(filtro.ToUpper())
+                                select c;
+
+
+            return listaFiltrada.ToList();
+        }
+    }
+}
---
+++ ViewModels/VeiculosViewModel.cs
@@ -0,0 +1,52 @@
+﻿using ConsultaVeiculos.Models;
+using ConsultaVeiculos.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ConsultaVeiculos.ViewModels
+{
+    public class VeiculosViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private string _marca;
+        public string Marca
+        {
+            get => _marca;
+            set{
+                if (_marca != value)
+                {
+                    _marca = value;
+                    NotifyPropertyChanged("Marca");
+                }
+            }
+        }
+
+        public ObservableCollection<Veiculos> Veiculos { get; set; }
+
+        public ICommand BuscarVeiculos {  get; set; }
+
+        public VeiculosViewModel() 
+        {
+            BuscarVeiculos = new RelayCommand(ObterVeiculos);
+        }
+
+        public void ObterVeiculos(object obj)
+        {
+            var veiculosList = new Veiculos().ObterVeiculos(Marca);
+            Veiculos = new ObservableCollection<Veiculos>(veiculosList as List<Veiculos>);
+            NotifyPropertyChanged("Veiculos");
+        }
+
+        public void NotifyPropertyChanged(string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
